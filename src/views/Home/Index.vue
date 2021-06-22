e0b6d95a1ebe9d182f1ce9231ea12e7f2860e421
Jorman <jespinoza@glamit.com.ar>
2021-06-22 00:35:15 -0300

    Main form

---
+++ src/views/Home/Index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <HomeTitle/>
+
+    <MainForm/>
+  </div>
+</template>
+
+<script>
+import HomeTitle from '@/views/Home/HomeTitle'
+import MainForm from '@/views/Home/HomeForm'
+
+export default {
+  name: 'Home',
+  components: {
+    HomeTitle,
+    MainForm
+  }
+}
+</script>
